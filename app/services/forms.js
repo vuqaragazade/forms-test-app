5468fd8cb89e2f4beb8b4a89db965bcd9d2201be
ampatspell <ampatspell@gmail.com>
2021-09-01 17:06:23 +0300

    cleanup

--- app/services/forms.js
+++ app/services/forms.js
@@ -1,22 +1,16 @@
 import Service from '@ember/service';
 import definitions from '../lib/definitions';
 import { inject as service } from '@ember/service';
+import { assert } from '@ember/debug';
 
 export default class FormsService extends Service {
 
   @service models;
 
-  _loadDefinition(type) {
-    return definitions[type];
-  }
-
-  _createModel(type, definition) {
-    return this.models.create(`forms/${type}`, { type, definition });
-  }
-
-  async createModel(type) {
-    let definition = this._loadDefinition(type);
-    let model = this._createModel(type, definition);
+  createModel(type) {
+    let definition = definitions[type];
+    assert(`definition for type '${type}' is not registered`, definition);
+    let model = this.models.create(`forms/${type}`, { type, definition });
     return model;
   }
 
--- app/services/models.js
+++ app/services/models.js
@@ -1,11 +1,13 @@
 import Service from '@ember/service';
 import { getOwner } from '@ember/application';
+import { assert } from '@ember/debug';
 
 export default class ModelsService extends Service {
 
   create(name, ...args) {
     let owner = getOwner(this);
-    let factory = owner.factoryFor(`model:${name}`).class;
+    let factory = owner.factoryFor(`model:${name}`)?.class;
+    assert(`model '${name}' is not registered`, factory);
     return new factory(owner, ...args);
   }
 
